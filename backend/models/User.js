bf9736feeec870e2d20ca16ea6f866dfd4431619
umesshk <umeshkumar153654@gmail.com>
2025-10-24 23:23:26 +0530

    login and register route

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose';
-
 const userSchema = new mongoose.Schema({
-  userId: { type: String, required: true, unique: true },
-  displayName: { type: String, required: true },
+  name: { type: String, required: true },
+  email :{type: String, required: true}, 
   avatarUrl: { type: String },
   passwordHash: { type: String, required: true },
   preferences: {
